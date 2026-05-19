2f16724e7e1d2c32f6cd3690436474a82d78a28a
DauPham <dau.pham@codeit.com.vn>
2026-05-19 14:20:13 +0700

    Fix resource schema

--- apps/pms-api/src/models/Resource.js
+++ apps/pms-api/src/models/Resource.js
@@ -6,12 +6,11 @@ const { Schema } = mongoose;
 const ResourceSchema = new Schema(
   {
     _id: {
-      type: String,
-      required: true
+      type: String
     },
     name: {
       type: String,
-      // required: true,
+      required: true,
       maxlength: 100,
       trim: true
     },
@@ -26,25 +25,21 @@ const ResourceSchema = new Schema(
   { timestamps: true }
 );
 
-ResourceSchema.pre('save', function (next) {
-  console.log('this:', this);
+ResourceSchema.pre('save', async function (next) {
   if (this.isNew && !this._id && this.name) {
     this._id = slugify(this.name);
-    console.log('this._id: ', this._id);
-    // const resource = await Resource.findOne({ _id: this._id });
-    // console.log('resource: ', resource);
-    // if (resource) {
-    //   let number = +resource.id.split('-').at(-1);
-    //   if (!number) number = 0;
-    //   console.log(`number:`, number);
-    //   this._id += '-' + ++number;
-    // }
+    const resource = await Resource.findOne({ name: this.name }).sort({
+      createdAt: -1
+    });
+    if (resource) {
+      let number = +resource._id.split('-').at(-1);
+      if (!number) number = 0;
+      this._id += '-' + ++number;
+    }
   }
 
   next();
 });
-
-ResourceSchema.index({ name: 1 });
 
 export const Resource =
   mongoose.models.Resource ?? mongoose.model('Resource', ResourceSchema);
